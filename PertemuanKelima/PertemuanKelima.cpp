30350a70a8f307a8ea3f0f3340090e8d1e296c9e
ImronSatrioPurnomo <satriogames2223@gmail.com>
2024-03-25 10:15:42 +0700

    penmbahan do

--- PertemuanKelima/PertemuanKelima.cpp
+++ PertemuanKelima/PertemuanKelima.cpp
@@ -27,8 +27,28 @@ int pembagian(int bil_1, int bil_2) {
     return bil_1 / bil_2;
 }
 
+
+
 int main()
 {
-    
+    int pilihan;
+
+    do {
+        system("cls");
+        cout << "========== menu ============" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukan menu yang ingin di pilih (1/2/3/4/5): ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1;
+        }
+
+
+    }
 }
 
